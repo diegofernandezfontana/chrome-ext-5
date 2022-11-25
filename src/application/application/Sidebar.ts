449174026479b099311ab5f1704cc1e909bae519
Diego <diego.fernandez@manomano.com>
2022-11-25 15:46:11 +0100

    Refactor renderContent so sidebar

--- src/application/application/Sidebar.ts
+++ src/application/application/Sidebar.ts
@@ -1,20 +1,47 @@
+import browser, { Browser } from "webextension-polyfill";
 export class Sidebar {
-  isOpen = false;
-
   public appRoot: HTMLDivElement;
-  public root: HTMLDivElement;
+  public rootElement: HTMLDivElement;
+  public cssPaths: string[];
+  public shadowRoot: ShadowRoot;
 
-  constructor() {
+  constructor(cssPaths: string[]) {
+    this.cssPaths = cssPaths;
+
+    this.rootElement = document.createElement("div");
+
+    this.shadowRoot = this.rootElement.attachShadow({
+      mode: import.meta.env.DEV ? "open" : "closed",
+    });
+
     this.appRoot = document.createElement("div");
-    this.root = document.createElement("div");
+
+    this.addStyleToShadowRoot();
   }
 
-  toggleIsOpen() {
-    this.isOpen = !this.isOpen;
+  private async addStyleToShadowRoot() {
+    if (import.meta.hot) {
+      const { addViteStyleTarget } = await import(
+        "@samrum/vite-plugin-web-extension/client"
+      );
+
+      await addViteStyleTarget(this.shadowRoot);
+    } else {
+      this.cssPaths.forEach((cssPath: string) => {
+        const styleEl = document.createElement("link");
+        styleEl.setAttribute("rel", "stylesheet");
+        styleEl.setAttribute("href", browser.runtime.getURL(cssPath));
+        this.shadowRoot.appendChild(styleEl);
+      });
+    }
   }
 
-  appendAppToSidebar(render: (dom: any) => void) {
-    render(this.appRoot);
+  public getRootElementWithShadowRootAndAppInserted(): HTMLDivElement {
+    this.shadowRoot.appendChild(this.appRoot);
+    this.addStyleToMainSidebar();
+    this.addStyleToRootElement();
+
+    return this.rootElement;
   }
 
   render(renderFn: (div: any) => void) {
@@ -26,14 +53,14 @@ export class Sidebar {
     this.appRoot.style.border = "3px solid blue";
   }
 
-  public addStyleToMainSidebar = (container: HTMLElement) => {
-    container.style.position = "absolute";
-    container.style.width = "400px";
-    container.style.height = "50%";
-    container.style.border = "2px solid red";
-    container.style.display = "block";
-    container.style.top = "0px";
-    container.style.maxHeight = "100%";
-    container.style.zIndex = "1100"; //akhq sidebar has 1006
+  public addStyleToMainSidebar = () => {
+    this.rootElement.style.position = "absolute";
+    this.rootElement.style.width = "400px";
+    this.rootElement.style.height = "50%";
+    this.rootElement.style.border = "2px solid red";
+    this.rootElement.style.display = "block";
+    this.rootElement.style.top = "0px";
+    this.rootElement.style.maxHeight = "100%";
+    this.rootElement.style.zIndex = "1100"; //akhq sidebar has 1006
   };
 }
--- src/entries/contentScript/renderContent.ts
+++ src/entries/contentScript/renderContent.ts
@@ -3,39 +3,11 @@ import { Sidebar } from "~/application/application/Sidebar";
 
 export default async function renderContent(
   cssPaths: string[],
-  renderExtension: (appRoot: HTMLElement) => void
+  renderExtensionFn: (appRoot: HTMLElement) => void
 ) {
-  const sidebar = new Sidebar();
-
-  const rootElement = document.createElement("div");
-
-  const shadowRoot = rootElement.attachShadow({
-    mode: import.meta.env.DEV ? "open" : "closed",
-  });
-
-  if (import.meta.hot) {
-    const { addViteStyleTarget } = await import(
-      "@samrum/vite-plugin-web-extension/client"
-    );
-
-    await addViteStyleTarget(shadowRoot);
-  } else {
-    cssPaths.forEach((cssPath: string) => {
-      const styleEl = document.createElement("link");
-      styleEl.setAttribute("rel", "stylesheet");
-      styleEl.setAttribute("href", browser.runtime.getURL(cssPath));
-      shadowRoot.appendChild(styleEl);
-    });
-  }
-  //rootElement
-  // Shadow root
-  // appRoot
-
-  const appRoot = sidebar.appRoot;
-  shadowRoot.appendChild(appRoot);
-  sidebar.addStyleToMainSidebar(rootElement);
-  sidebar.addStyleToRootElement();
+  const sidebar = new Sidebar(cssPaths);
+  const rootElement = sidebar.getRootElementWithShadowRootAndAppInserted();
   document.body.appendChild(rootElement);
 
-  sidebar.render(renderExtension);
+  sidebar.render(renderExtensionFn);
 }
